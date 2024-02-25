98233f476c3720ae5245f8acdfe05b8eb047ca0b
Khaleed <ikhwankhalid@student.telkomuniversity.ac.id>
2024-02-26 00:35:49 +0700

    initial commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TansformationsPage = () => {
+    return (
+        <div>
+            TransformationsPage
+        </div>
+    );
+};
+
+export default TansformationsPage;
